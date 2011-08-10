630f9925d09378d1fd4dfd643c3f55c6d6890838
Alex Harin <aharin@thoughtworks.com>
2011-08-10 11:44:57 +0100

    Fixing relative path issue for multi-module maven build

--- scct/src/main/scala/reaktor/scct/report/HtmlReporter.scala
+++ scct/src/main/scala/reaktor/scct/report/HtmlReporter.scala
@@ -1,13 +1,29 @@
 package reaktor.scct.report
 
-import java.io.File
-import io.Source
-import xml.{Text, Node, NodeSeq}
 import reaktor.scct._
 
 object HtmlReporter {
-  def report(blocks: List[CoveredBlock], env: Env) =
-    new HtmlReporter(new CoverageData(blocks), new HtmlReportWriter(env.reportDir), env).report
+
+
+  def report(blocks: List[CoveredBlock], env: Env)  = {
+
+    def fixRelativePathForMaven(sourceFile: String) : String = {
+      val prefix: String = env.projectId + "/"
+
+      if (sourceFile.startsWith(prefix)) sourceFile.substring(prefix.length, sourceFile.length)
+      else sourceFile
+    }
+
+    def fixRelativePathInCoveredBlockForMaven(block:CoveredBlock): CoveredBlock = {
+      val name: Name = block.name
+      val nameWithFixedRelativePath: Name = Name(fixRelativePathForMaven(name.sourceFile), name.classType, name.packageName, name.className)
+      val blockWithFixedRelativePath: CoveredBlock =  new CoveredBlock(block.id, nameWithFixedRelativePath, block.offset, block.placeHolder)
+      blockWithFixedRelativePath.count = block.count
+      blockWithFixedRelativePath
+    }
+
+    new HtmlReporter(new CoverageData(blocks.map(fixRelativePathInCoveredBlockForMaven)), new HtmlReportWriter(env.reportDir), env).report
+  }
 }
 
 class HtmlReporter(data: CoverageData, writer: HtmlReportWriter, env: Env) extends HtmlHelper {
@@ -36,26 +52,26 @@ class HtmlReporter(data: CoverageData, writer: HtmlReportWriter, env: Env) exten
     val html =
       <div class="content">
         <div class="pkgRow header">
-          <a href={files.summary}>Summary { format(data.percentage) }</a>
-        </div>
-        {
-          for ((pkg, packageData) <- data.forPackages) yield {
-            <div class="pkgRow pkgLink">
-              <a href={packageReportFileName(pkg)}>
-                { pkg }&nbsp;{ format(packageData.percentage) }
+          <a href={files.summary}>Summary
+            {format(data.percentage)}
+          </a>
+        </div>{for ((pkg, packageData) <- data.forPackages) yield {
+        <div class="pkgRow pkgLink">
+          <a href={packageReportFileName(pkg)}>
+            {pkg}&nbsp;{format(packageData.percentage)}
+          </a>
+        </div> ++
+          <div class="pkgRow pkgContent">
+            {for ((clazz, classData) <- packageData.forClasses) yield
+            <div class="pkgRow">
+              <a href={classHref(clazz)}>
+                <span class="className">
+                  {classNameHeader(clazz)}
+                </span> &nbsp;{format(classData.percentage)}
               </a>
-            </div> ++
-            <div class="pkgRow pkgContent">
-              { for ((clazz, classData) <- packageData.forClasses) yield
-                  <div class="pkgRow">
-                    <a href={ classHref(clazz) }>
-                      <span class="className">{ classNameHeader(clazz) }</span>&nbsp;{ format(classData.percentage) }
-                    </a>
-                  </div>
-              }
-            </div>
-          }
-        }
+            </div>}
+          </div>
+      }}
       </div>
     writer.write(files.packages, html)
   }
@@ -71,15 +87,19 @@ class HtmlReporter(data: CoverageData, writer: HtmlReportWriter, env: Env) exten
   def resources {
     val rs = List("class.png", "object.png", "package.png", "trait.png", "filter_box_left.png", "filter_box_right.png",
       "jquery-1.6.1.min.js", "jquery-ui-1.8.4.custom.min.js", "style.css", "main.js", "index.html")
-    rs.foreach { name =>
-      writer.write(name, IO.readResourceBytes("/html-reporting/"+name))
+    rs.foreach {
+      name =>
+        writer.write(name, IO.readResourceBytes("/html-reporting/" + name))
     }
   }
+
   def sourceFileReports {
     for ((sourceFile, sourceData) <- data.forSourceFiles) {
+
       val report = SourceFileHtmlReporter.report(sourceFile, sourceData, env)
       writer.write(sourceReportFileName(sourceFile), report)
     }
   }
+
 
 }
